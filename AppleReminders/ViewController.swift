3ab8333790e7ecbff7c9efe72ced8bdd3b6b4422
Josh R <seventytwoapp@gmail.com>
2020-01-24 17:45:57 -0600

    Initial Commit

---
+++ AppleReminders/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppleReminders
+//
+//  Created by Josh R on 1/24/20.
+//  Copyright © 2020 Josh R. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
