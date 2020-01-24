da024dd43339162af0947efe1cfcc8404e1c8c24
Josh R <seventytwoapp@gmail.com>
2020-01-24 17:51:06 -0600

    Project start

--- AppleReminders/MainVC.swift
+++ AppleReminders/MainVC.swift
@@ -8,11 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
